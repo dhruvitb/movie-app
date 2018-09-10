89b57614f2fec30dfff2611bb0b219b44ac6aa28
dhruvit <bhavsar.dhruvit@gmail.com>
2018-09-10 13:40:33 -0400

    move fetching to constructor for better performance

--- src/layout/movies/movies.jsx
+++ src/layout/movies/movies.jsx
@@ -6,16 +6,17 @@ import './movies.css'
 
 class Movies extends Component {
 
-    state = {
-        moviesData: null
-    }
-
-    componentDidMount() {
+    constructor() {
+        super();
         const topMoviesURL = `https://api.themoviedb.org/3/discover/movie?api_key=${authToken}&language=en-US&sort_by=popularity.desc&page=1`
         axios.get(topMoviesURL).then(response => {
             this.setState({moviesData: response.data.results})
             console.log(this.state.moviesData);
         })
+    }
+
+    state = {
+        moviesData: null
     }
 
     render() {
